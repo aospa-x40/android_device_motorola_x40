cdc5b708594a14c0b6ad27676218880b6a2c2a20
Jake Weinstein <jake@aospa.co>
2022-07-20 10:07:19 -0500

    x40: BoardConfig: Drop BOARD_SYSTEMSDK_VERSIONS := 33
    This is not necessary.
    
    Change-Id: Ifbc6e173e8fd2b2deff59c61840d88276999565f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,8 +4,6 @@
 #
 # TODO(b/124534788): Temporarily allow eng and debug LOCAL_MODULE_TAGS
 
-BOARD_SYSTEMSDK_VERSIONS := 33
-
 TARGET_BOARD_PLATFORM := kalama
 TARGET_BOOTLOADER_BOARD_NAME := kalama
 
