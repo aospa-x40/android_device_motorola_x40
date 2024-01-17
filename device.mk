dc424a9716589be38d51bcc38458a6544935a523
Kunmun <kunmun@aospa.co>
2024-01-17 12:46:32 +0530

    x40: Makefile: Drop kernel modules install path definitions
    This is done in DLKM component of qcom common.
    
    Change-Id: I2d160c19b611187015ed672e07b5d268f65d0f4d

--- device.mk
+++ device.mk
@@ -155,10 +155,6 @@ TARGET_USES_IMAGE_GEN_TOOL := true
 DEVICE_MANIFEST_SKUS := kalama
 DEVICE_MANIFEST_KALAMA_FILES := $(DEVICE_PATH)/manifest_kalama.xml
 
-# Kernel modules install path
-KERNEL_MODULES_INSTALL := dlkm
-KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTALL)/lib/modules
-
 #Enable full treble flag
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
